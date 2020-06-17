92237ac8cd5f2de03391d72df5a98a6e151a802e
Franklin Huichi <frahuichic@upt.pe>
2020-06-16 20:53:43 -0500

    Agregando Api

---
+++ app.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3050;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'node20_mysql'
+});
+
+//ROUTE
+app.get('/', (req, res) =>{
+    res.send('Welcome to my API');
+});
+
+app.get('/customers', (req, res) =>{
+    const sql = 'SELECT * FROM customers';
+    connection.query(sql, (error, results) => {
+        if(error) throw error;
+        if(results.length > 0){
+            res.json(results);
+        }else{
+            res.send("Not result");
+        }
+    });
+});
+
+app.get('/customers/:id', (req, res) =>{
+    const {id} = req.params;
+    const sql = `SELECT * FROM customers WHERE id=${id}`;
+    connection.query(sql, (error, result) =>{
+        if(error) throw (error);
+        if(result.length > 0){
+            res.json(result);
+        }else{
+            res.send('Not result');
+        }
+    });
+});
+
+app.post('/add', (req,res) => {
+    const sql = 'INSERT INTO customers SET ?'
+    const customerObj = {
+        name: req.body.name,
+        city:req.body.city
+    };
+
+    connection.query(sql, customerObj, error=>{
+        if(error) throw error;
+        res.send('Customer created!');
+    });
+});
+
+app.put('/update/:id', (req,res) => {
+    const {id} = req.params;
+    const {name, city} = req.body;
+    const sql = `UPDATE customers SET name = '${name}', city = '${city}' WHERE id = ${id}`
+
+    connection.query(sql, error =>{
+        if(error) throw error;
+        res.send("Update customer");
+    });
+});
+
+app.delete('/delete/:id', (req,res) =>{
+    const {id} = req.params;
+    const sql = `DELETE FROM customers WHERE id = ${id}`;
+
+    connection.query(sql, error =>{
+        if(error) throw error;
+        res.send('Customer deleted!');
+    })
+});
+connection.connect(error =>{
+    if(error) throw error;
+    console.log('Database server running !');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
